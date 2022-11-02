af6f9819b38299196ea34e44ad2d39a55acd4e48
Marsel <42341360+binkluv@users.noreply.github.com>
2022-11-02 16:12:47 +0300

    Step-1

--- app/src/main/java/ru/mmnigmatullov/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/mmnigmatullov/weatherapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ru.mmnigmatullov.weatherapp.screens.MainScreen
 import ru.mmnigmatullov.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
-    }
-}
